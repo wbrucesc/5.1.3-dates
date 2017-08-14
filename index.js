0341d48a7f7a7356ae196bcc1523dfa4fe874b09
Will Bruce <willtiger13@gmail.com>
2017-08-14 16:12:28 -0400

    need to add chalk

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
 var moment = require("moment");
 
 
-
-// var today = new Date();
-// console.log(today);
-// var weekday = today.getDay();
-// console.log(weekday);
-// var month = today.getMonth();
-// console.log(month);
-// var dayOfMonth = today.getDate();
-// console.log(dayOfMonth);
-// var year = today.getFullYear();
-// console.log(year);
-// var hours = today.getHours();
-// console.log(hours);
-// var mins = today.get
-
-// function getDate(){
-//   // console.log("It is" + date);
-// }
 var pre = "It is ";
 
-var firstLine = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+var firstLine = moment().format("dddd, MMMM Do YYYY, hh:mm:ss a");
 var secondLine = moment().format("DDDo");
-var thirdLine = moment().startOf('day').fromNow();
-console.log(thirdLine);
+var hours = moment().format("hh");
+var mins = moment().format("mm");
+var secs = moment().format("ss");
+var timeInSeconds = (hours * 3600) + (mins * 60) + secs;
+var daylight = moment().isDST();
+var leap = moment().isLeapYear();
 
 
+function timeOfYear(daylight){
+  if (daylight === true){
+    return pre + "during Daylight Savings Time.";
+  } else {
+    return pre + "not Daylight Savings Time.";
+  }
+}
 
-
-
-
+function feelingFroggy(leap){
+  if (leap === true){
+    return pre + "a leap year.";
+  } else {
+    return pre + "not a leap year.";
+  }
+}
 
 
 console.log(pre + firstLine + ".");
 console.log(pre + "the " + secondLine + " day of the year.");
-console.log(pre + thirdLine + "into the day");
+console.log(pre + timeInSeconds +
+" seconds into the day.");
+console.log(timeOfYear(daylight));
+console.log(feelingFroggy(leap));
